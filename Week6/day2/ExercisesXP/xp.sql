d667dd67942a05a1bab228516e2af152e5743a4f
kimkaye <kimkaye555@icloud.com>
2022-01-12 11:17:06 +0200

    day2

--- Week6/day2/ExercisesXP/xp.sql
+++ Week6/day2/ExercisesXP/xp.sql
@@ -144,5 +144,83 @@ INNER JOIN customers ON customer_id = customers_id)
 INNER JOIN items ON item_id = items_id);
 
 --Exercise 2 : Dvdrental Database
+--In the dvdrental database write a query to select all the columns from the “customer” table.
 
+SELECT * FROM customer;
 
+--Write a query to display the names (first_name, last_name) using an alias named “full_name”.
+
+SELECT first_name AS alias_last_name, last_name AS alias_name
+FROM customer;
+
+--Lets get all the dates that accounts were created.
+-- Write a query to select all the create_date from the “customer” table (there should be no duplicates).
+
+SELECT DISTINCT create_date FROM customer;
+
+--Write a query to get all the customer details from the customer table,
+--it should be displayed in descending order by their first name.
+
+SELECT * FROM customer
+ORDER BY first_name;
+
+--Write a query to get the film ID, title, description, year of release and rental rate in ascending order according to their rental rate.
+
+SELECT *
+FROM film
+ORDER BY rental_rate ASC;
+
+--Write a query to get the address, and the phone number of all customers living in the Texas district, these details can be found in the “address” table.
+SELECT district, phone
+FROM address
+WHERE district='Texas';
+
+--Write a query to retrieve all movie details where the movie id is either 15 or 150.
+SELECT *
+FROM film
+WHERE film_id='15' OR film_id='50';
+
+--Write a query which should check if your favorite movie exists in the database.
+-- Have your query get the film ID, title, description, length and the rental rate, these details can be found in the “film” table.
+
+SELECT *
+FROM film
+WHERE title='Alien Center';
+
+--No luck finding your movie? Maybe you made a mistake spelling the name. Write a query to get the film ID, title, description, length and the rental rate of all the movies starting with the two first letters of your favorite movie.
+
+SELECT *
+FROM film
+WHERE title LIKE 'Al%';
+
+--Write a query which will find the 10 cheapest movies.
+SELECT * FROM film
+ORDER BY replacement_cost ASC
+LIMIT 10;
+
+--Not satisfied with the results. Write a query which will find the next 10 cheapest movies.
+SELECT * FROM film
+ORDER BY replacement_cost ASC
+LIMIT 20;
+
+--Write a query which will join the data in the customer table and the payment table.
+--You want to get the amount and the date of every payment made by a customer, ordered by their id (from 1 to…).
+SELECT DISTINCT payment.amount, payment.payment_date, payment.customer_id
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id
+ORDER BY customer_id;
+
+--You need to check your inventory. Write a query to get all the movies which are not in inventory.
+
+SELECT film.film_id, film.title
+FROM film
+LEFT JOIN inventory ON inventory.film_id = film.film_id
+WHERE inventory.film_id IS NULL;
+
+--Write a query to find which city is in which country.
+
+SELECT DISTINCT city.city, country.country
+FROM city
+INNER JOIN country
+ON city.country_id = country.country_id;
